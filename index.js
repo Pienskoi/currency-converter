66b0c3551c9e823eb575dda46ec15d97d6075e48
Pienskoi <vladymyrpenskoy@gmail.com>
2020-06-04 23:49:06 +0300

    Change arguments to object

--- index.js
+++ index.js
@@ -3,27 +3,31 @@
 const https = require('https');
 const url = 'https://api.exchangeratesapi.io';
 
-const convert = (value, from, to, day = 'latest') => {
-  return new Promise((resolve, reject) => {
-    https.get(`${url}/${day}?base=${from}`, res => {
-      const statusCode = res.statusCode;
-      if (statusCode !== 200) {
-        const error = new Error('Request failed.\nStatus code: ' + statusCode);
-        console.error(error.message);
-        res.resume();
-        return;
-      }
+const convert = options => new Promise((resolve, reject) => {
+  const value = options.value || 1;
+  const from = options.from;
+  const to = options.to;
+  const day = options.day || 'latest';
 
-      let data = '';
-      res.on('data', chunk => { data += chunk; });
-      res.on('end', () => {
-        const parsedData = JSON.parse(data);
-        const rate = parsedData.rates[to];
-        const convertedValue = value * rate;
-        resolve(convertedValue);
-      });
-    }).on('error', e => {
-      reject(e);
+  https.get(`${url}/${day}?base=${from}`, res => {
+    const statusCode = res.statusCode;
+    if (statusCode !== 200) {
+      const error = new Error('Request failed.\nStatus code: ' + statusCode);
+      console.error(error.message);
+      res.resume();
+      return;
+    }
+    let data = '';
+    res.on('data', chunk => { data += chunk; });
+    res.on('end', () => {
+      const parsedData = JSON.parse(data);
+      const rate = parsedData.rates[to];
+      const convertedValue = value * rate;
+      resolve(convertedValue);
     });
+  }).on('error', e => {
+    reject(e);
   });
-};
+});
+
+module.exports = convert;
