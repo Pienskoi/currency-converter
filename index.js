13ed5c7f75476797e7b1813a17ecf7e39b894286
Pienskoi <vladymyrpenskoy@gmail.com>
2020-06-05 01:00:02 +0300

    Add logging specific errors

--- index.js
+++ index.js
@@ -4,26 +4,38 @@ const https = require('https');
 const url = 'https://api.exchangeratesapi.io';
 
 const convert = options => new Promise((resolve, reject) => {
-  const value = options.value || 1;
   const from = options.from;
   const to = options.to;
+  const value = options.value || 1;
   const day = options.day || 'latest';
+  if (!from) {
+    reject(new Error('No currency to convert from.'));
+  } else if (!to) {
+    reject(new Error('No currency to convert to.'));
+  } else if (typeof value !== 'number') {
+    reject(new Error('Value to convert is not a number.'));
+  }
 
   https.get(`${url}/${day}?base=${from}`, res => {
-    const statusCode = res.statusCode;
-    if (statusCode !== 200) {
-      const error = new Error('Request failed.\nStatus code: ' + statusCode);
-      console.error(error.message);
-      res.resume();
-      return;
+    const code = res.statusCode;
+    if (code !== 200 && code !== 400) {
+      reject(new Error('Request failed.\nStatus code: ' + code));
+      res.resume;
     }
+
     let data = '';
     res.on('data', chunk => { data += chunk; });
     res.on('end', () => {
       const parsedData = JSON.parse(data);
-      const rate = parsedData.rates[to];
-      const convertedValue = value * rate;
-      resolve(convertedValue);
+      if (parsedData.error) {
+        reject(new Error(parsedData.error));
+      } else if (!Object.keys(parsedData.rates).includes(to)) {
+        reject(new Error(`Currency '${to}' is not supported.`));
+      } else {
+        const rate = parsedData.rates[to];
+        const convertedValue = value * rate;
+        resolve(convertedValue);
+      }
     });
   }).on('error', e => {
     reject(e);
