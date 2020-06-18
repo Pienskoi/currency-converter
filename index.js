fd3c7e4d6326ba17317dcf9ade4cfa52887e4c27
Pienskoi <vladymyrpenskoy@gmail.com>
2020-06-18 04:56:12 +0300

    Add object destructuring in function parameter

--- index.js
+++ index.js
@@ -3,36 +3,31 @@
 const https = require('https');
 const url = 'https://api.exchangeratesapi.io';
 
-const convert = options => new Promise((resolve, reject) => {
-  const from = options.from;
-  const to = options.to;
-  let value;
-  if (!Object.keys(options).includes('value')) value = 1;
-  else value = options.value;
-  const day = options.day || 'latest';
-
-  https.get(`${url}/${day}?base=${from}`, res => {
-    const code = res.statusCode;
-    if (code !== 200) {
-      reject(new Error(`HTTP status code ${code}`));
-      res.resume;
-    }
-
-    let data = '';
-    res.on('data', chunk => { data += chunk; });
-    res.on('end', () => {
-      try {
-        const parsedData = JSON.parse(data);
-        const rate = parsedData.rates[to];
-        const convertedValue = value * rate;
-        resolve(convertedValue);
-      } catch (err) {
-        reject(err);
+const convert = ({ value = 1, from, to, day = 'latest' }) => {
+  return new Promise((resolve, reject) => {
+    https.get(`${url}/${day}?base=${from}`, res => {
+      const code = res.statusCode;
+      if (code !== 200) {
+        reject(new Error(`HTTP status code ${code}`));
+        res.resume;
       }
+
+      let data = '';
+      res.on('data', chunk => { data += chunk; });
+      res.on('end', () => {
+        try {
+          const parsedData = JSON.parse(data);
+          const rate = parsedData.rates[to];
+          const convertedValue = value * rate;
+          resolve(convertedValue);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    }).on('error', e => {
+      reject(e);
     });
-  }).on('error', e => {
-    reject(e);
   });
-});
+};
 
 module.exports = convert;
