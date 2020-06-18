670ee4808a0c326db79e14b9ea28a56e29c531b4
Pienskoi <vladymyrpenskoy@gmail.com>
2020-06-18 05:11:36 +0300

    Split into two functions

--- index.js
+++ index.js
@@ -2,34 +2,37 @@
 
 const https = require('https');
 
-const convert = ({ value = 1, from, to, day = 'latest' }) => {
+const requestData = (baseCurrency, date) => new Promise((resolve, reject) => {
   const url = 'https://api.exchangeratesapi.io';
-  const path = `/${day}?base=${from}`;
+  const path = `/${date}?base=${baseCurrency}`;
 
-  return new Promise((resolve, reject) => {
-    https.get(url + path, res => {
-      const code = res.statusCode;
-      if (code !== 200) {
-        reject(new Error(`HTTP status code ${code}`));
-        res.resume;
+  https.get(url + path, res => {
+    const code = res.statusCode;
+    if (code !== 200) {
+      reject(new Error(`HTTP status code ${code}`));
+      res.resume;
+    }
+
+    let data = '';
+    res.on('data', chunk => { data += chunk; });
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(data);
+        resolve(parsedData);
+      } catch (err) {
+        reject(err);
       }
-
-      let data = '';
-      res.on('data', chunk => { data += chunk; });
-      res.on('end', () => {
-        try {
-          const parsedData = JSON.parse(data);
-          const currencyRate = parsedData.rates[to];
-          const convertedValue = value * currencyRate;
-          resolve(convertedValue);
-        } catch (err) {
-          reject(err);
-        }
-      });
-    }).on('error', err => {
-      reject(err);
     });
+  }).on('error', err => {
+    reject(err);
   });
+});
+
+const convert = async ({ value = 1, from, to, day = 'latest' }) => {
+  const data = await requestData(from, day);
+  const currencyRate = data.rates[to];
+  const convertedValue = value * currencyRate;
+  return convertedValue;
 };
 
 module.exports = convert;
