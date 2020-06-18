18a89e163a8b3201d504137f2179a401e4967d72
Pienskoi <vladymyrpenskoy@gmail.com>
2020-06-18 04:12:28 +0300

    Remove code duplication

--- consoleapp.js
+++ consoleapp.js
@@ -8,42 +8,32 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const question1 = () =>  new Promise(resolve => {
-  rl.question('Value to convert: (1) ', answ => resolve(answ));
-});
-const question2 = () =>  new Promise(resolve => {
-  rl.question('From: ', answ => resolve(answ));
-});
-const question3 = () =>  new Promise(resolve => {
-  rl.question('To: ', answ => resolve(answ));
-});
-const question4 = () =>  new Promise(resolve => {
-  rl.question('Date(YYYY-MM-DD): (latest) ', answ => resolve(answ));
+const askQuestion = question => new Promise(resolve => {
+  rl.question(question, answer => resolve(answer));
 });
 
-const args = {};
+const questions = [
+  'Value to convert: (1) ',
+  'From(currency code): ',
+  'To(currency code): ',
+  'Date(YYYY-MM-DD): (latest) '
+];
 
-const askQuestions = async callback => {
-  const value = (await question1()).trim();
-  const from = (await question2()).trim();
-  const to = (await question3()).trim();
-  args.day = (await question4()).trim();
-  if (value === '') args.value = 1;
-  else args.value = parseFloat(value);
-  args.from = from.toUpperCase();
-  args.to = to.toUpperCase();
-  try {
-    const convertedValue = await convert(args);
-    callback(convertedValue);
-  } catch (e) {
-    console.error(e);
+const getConvertedValue = async () => {
+  const answers  = [];
+  for (const question of questions) {
+    const answer = await askQuestion(question);
+    answer.trim();
+    answers.push(answer);
   }
   rl.close();
+  const [value, from, to, day] = answers;
+  const parameters = { from, to };
+  if (value !== '') parameters.value = parseFloat(value);
+  if (day !== '') parameters.day = day;
+  const convertedValue = await convert(parameters);
+  const result = `${value} ${from} is equal ${convertedValue} ${to}`;
+  console.log(result);
 };
 
-const parseValue = val => {
-  const parsedVal = parseFloat(val);
-  console.log(`${args.value} ${args.from} equals ${parsedVal} ${args.to}`);
-};
-
-askQuestions(parseValue);
+getConvertedValue();
