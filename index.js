f75b7b53987ab521a400cd27eccb0c0cc8de4c19
Pienskoi <vladymyrpenskoy@gmail.com>
2020-06-18 04:27:48 +0300

    Remove foolproof error handling

--- index.js
+++ index.js
@@ -10,18 +10,11 @@ const convert = options => new Promise((resolve, reject) => {
   if (!Object.keys(options).includes('value')) value = 1;
   else value = options.value;
   const day = options.day || 'latest';
-  if (!from) {
-    reject(new Error('No currency to convert from.'));
-  } else if (!to) {
-    reject(new Error('No currency to convert to.'));
-  } else if (typeof value !== 'number' || Number.isNaN(value)) {
-    reject(new Error('Value to convert is not a number.'));
-  }
 
   https.get(`${url}/${day}?base=${from}`, res => {
     const code = res.statusCode;
-    if (code !== 200 && code !== 400) {
-      reject(new Error('Request failed.\nStatus code: ' + code));
+    if (code !== 200) {
+      reject(new Error(`HTTP status code ${code}`));
       res.resume;
     }
 
@@ -29,15 +22,9 @@ const convert = options => new Promise((resolve, reject) => {
     res.on('data', chunk => { data += chunk; });
     res.on('end', () => {
       const parsedData = JSON.parse(data);
-      if (parsedData.error) {
-        reject(new Error(parsedData.error));
-      } else if (!Object.keys(parsedData.rates).includes(to)) {
-        reject(new Error(`Currency '${to}' is not supported.`));
-      } else {
-        const rate = parsedData.rates[to];
-        const convertedValue = value * rate;
-        resolve(convertedValue);
-      }
+      const rate = parsedData.rates[to];
+      const convertedValue = value * rate;
+      resolve(convertedValue);
     });
   }).on('error', e => {
     reject(e);
